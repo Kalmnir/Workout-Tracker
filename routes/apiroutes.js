8e841280bc8cf203a59729ee18f16995e1e5419d
John-Mark Sackett <jmsackett91@gmail.com>
2021-10-08 09:56:11 -0400

    wrote the rest of my api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,5 +9,26 @@ module.exports = function (app) {
             });
     });
 
+    app.post('/api/workouts', function (req, res) {
+        console.log(req.body);
 
+        Workout.create({})
+            .then(function (data) {
+                res.json(data);
+            });
+    });
+
+    app.put('/api/workout/:id', function (req, res) {
+        Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+            .then(function (data) {
+                res.json(data);
+            });
+    });
+
+    app.get('/api/workouts/range', function (req, res) {
+        Workout.fin()
+            .then(function (data) {
+                res.json(data);
+            });
+    });
 }
